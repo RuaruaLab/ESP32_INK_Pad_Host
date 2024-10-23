d6eb137af24b616b765127a52bb0f766d10b5273
Yueluo <578903564@qq.com>
2024-10-24 02:12:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,47 +14,63 @@ def send_message(ser, cmd_id, data):
     print("Complete message:", message.hex())
     ser.write(message)
 
-# -------------------图片转换----------------------
-converter = ImageToEPaperBuffer("test_img/test6.jpg")
-converter.load_image()
-converter.resize_with_letterbox()
-converter.image_to_rgb_matrix()
-img_buffer = converter.process_image_to_buffer()
-converter.convert_image_to_4_colors()   #显示预览
+def send_image(path):
+    # -------------------图片转换----------------------
+    converter = ImageToEPaperBuffer(path)
+    converter.load_image()
+    converter.resize_with_letterbox()
+    converter.image_to_rgb_matrix()
+    img_buffer = converter.process_image_to_buffer()
+    converter.convert_image_to_4_colors()   #显示预览
+
+    # 发送名片
+    cmd_id = 0x01  # 命令ID
+
+    packet_size = 64
+    total_packets = len(img_buffer) // packet_size
+
+    for i in range(total_packets):
+        start_index = i * packet_size
+        end_index = start_index + packet_size
+
+        packet_data = img_buffer[start_index:end_index]
+        packet = bytearray([i % 256]) + bytearray(packet_data)  # 包编号&数据
+
+        send_message(ser, cmd_id, packet)  # CMD_ID = 0x01
+        time.sleep(0.0001)
+
+    remaining_data = len(img_buffer) % packet_size
+    if remaining_data > 0:
+        last_packet_data = img_buffer[-remaining_data:]
+        last_packet = bytearray([total_packets % 256]) + bytearray(last_packet_data)
+        send_message(ser, cmd_id, last_packet)
 
 
-# ------------------------串口发送-----------------------------
-ser = serial.Serial()
-ser.port = 'COM4'
-ser.baudrate = 115200
-ser.timeout = 1
+def send_wifi_credentials(ssid, password):
+    ssid_length = len(ssid)
+    password_length = len(password)
 
-ser.dtr = False
-ser.rts = False
+    if ssid_length > 32 or password_length > 64:
+        raise ValueError("SSID or password length exceeds the limit")
 
-ser.open()
+    data = bytearray([ssid_length, password_length]) + ssid.encode() + password.encode()
+    send_message(ser, 0x02, data)
 
-# 发送名片
-cmd_id = 0x01  # 命令ID
 
-packet_size = 64
-total_packets = len(img_buffer) // packet_size
+def main():
+    ser.open()
 
-for i in range(total_packets):
-    start_index = i * packet_size
-    end_index = start_index + packet_size
-    
-    packet_data = img_buffer[start_index:end_index]
-    packet = bytearray([i % 256]) + bytearray(packet_data)  # 包编号&数据
+    # send_image("test_img/test.jpg")
+    send_wifi_credentials("ssid", "password")
 
-    send_message(ser, 0x01, packet)  # CMD_ID = 0x01
-    time.sleep(0.0001)
+    ser.close()
 
-remaining_data = len(img_buffer) % packet_size
-if remaining_data > 0:
-    last_packet_data = img_buffer[-remaining_data:]
-    last_packet = bytearray([total_packets % 256]) + bytearray(last_packet_data)
-    send_message(ser, 0x01, last_packet)
+if __name__ == "__main__":
+    ser = serial.Serial()
+    ser.port = 'COM47'
+    ser.baudrate = 115200
+    ser.timeout = 1
+    ser.dtr = False
+    ser.rts = False
 
-# ser.write(bytearray([0x5c, 0x01, 0x7a]))
-ser.close()
+    main()
